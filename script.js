1777aafe5aa2fe982274bd2669a35b60911d98ec
Zeeshan Haider <ZeeshanHaider.1237750@gmail.com>
2026-06-06 14:19:28 +0500

    added the basics and some debugging according to the needs

--- script.js
+++ script.js
@@ -1,18 +1,115 @@
 const btns = document.querySelectorAll(".button");
-const display = document.getElementById("display");
-
-btns.forEach((button) => {
-    button.addEventListener('click', () =>{
-        display.textContent = button.textContent;
-    });
-});
-
+const displays = document.getElementById("display");
 const operator = document.querySelectorAll('#add, #subtract, #multiply, #divide');
-const num = document.querySelectorAll('#one, #two, #three, #four, #five, #six, #seven, #eight, #nine');
+const num = document.querySelectorAll('#zero, #one, #two, #three, #four, #five, #six, #seven, #eight, #nine');
 const AC = document.querySelector('#AC');
 const del = document.querySelector('#del');
 const percent = document.querySelector('#percent');
 const decimal = document.querySelector('#decimal-point');
 const sd = document.querySelector('#S-D');
 const signChange = document.querySelector('#sign-change');
+const equal = document.querySelector('#equal')
+
+let num1, num2;
+let currentValue = "";
+let previousValue = "";
+let currentOperator = "";
+let displaySymbol = "";
+
+let compute = (num1, operator, num2) => {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if(operator == "add"){
+        return  num1 + num2;
+    }
+    else if(operator == "subtract"){
+        return  num1 - num2;
+    }
+    else if(operator == "multiply"){
+        return  num1 * num2;
+    }
+    else if(operator == "divide"){
+        return  num1 / num2;
+    }
+}
+
+let updateDisplay = (text) => {
+    let displaySymbol = "";
+    if(currentOperator == "add"){
+        displaySymbol = "+";
+    }
+    else if(currentOperator == "subtract"){
+        displaySymbol = "-";
+    }
+    else if(currentOperator == "multiply"){
+        displaySymbol = "x";
+    }
+    else if(currentOperator == "divide"){
+        displaySymbol = "÷";
+    }
+    displays.textContent = previousValue+" "+displaySymbol+" "+currentValue;
+}
+
+let handleDigit = (digit) => {
+    currentValue += digit;
+    updateDisplay(currentValue);
+}
+
+let handleOperator =(nextOperator) => {
+    previousValue = currentValue;
+    currentValue = "";
+    currentOperator = nextOperator;
+}
+
+
+let handleEquals = () => {
+ let result = compute(previousValue, currentOperator, currentValue);
+ currentOperator = "";
+ previousValue = "";
+ currentValue = result;
+ updateDisplay(result);
+}
+
+let clearAll = () => {
+    currentValue = '';
+    previousValue = '';
+    currentOperator = '';
+    updateDisplay(0);
+}
+
+let deleteDigit = () => {
+   currentValue = currentValue.slice(0, -1);
+    updateDisplay(currentValue);
+}
+
+let handleDecimal = () => {
+    if(!currentValue.includes(".")){
+        currentValue += ".";
+        updateDisplay();
+    }
+}
+
+btns.forEach((button) => {
+    button.addEventListener('click', () =>{
+        if([...num].includes(button)){
+    handleDigit(button.textContent);
+}
+else if([...operator].includes(button)){
+    handleOperator(button.id);
+}
+else if(button===equal){
+    handleEquals();
+}
+else if(button===AC){
+    clearAll();
+}
+else if(button===del){
+    deleteDigit();
+}
+else if(button===decimal){
+     handleDecimal();
+}
+    });
+});
+
 
