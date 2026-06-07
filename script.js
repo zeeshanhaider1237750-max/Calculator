19cc521ec6ea90e48f3bc2af290bd3e98d3ad832
Zeeshan Haider <ZeeshanHaider.1237750@gmail.com>
2026-06-07 21:30:28 +0500

    Adding good features.

--- script.js
+++ script.js
@@ -15,8 +15,16 @@ let currentValue = "";
 let previousValue = "";
 let currentOperator = "";
 let displaySymbol = "";
-let calcOperator = "";
-3
+
+let findGCD = (a, b) => {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+};
+
 let compute = (num1, operator, num2) => {
     num1 = parseFloat(num1);
     num2 = parseFloat(num2);
@@ -60,42 +68,59 @@ let handleOperator =(nextOperator, nextOperatorSymbol) => {
        updateDisplay();
     }
 
-
-
 let handleEquals = () => {
     let addCount = [...currentOperator].filter(char => char === "+").length;
     let minusCount = [...currentOperator].filter(char => char === "-").length;
     let errorCount = [...currentOperator].filter(char =>char === "x" ||  char === "÷").length;
-   
+    let calcOperator = "";
+    if((currentOperator.includes("x") || currentOperator.includes("÷")) && currentOperator.length>1){
+     previousValue = "";
+     currentOperator = "";
+     currentValue = "";
+     updateDisplay("Syntax Error");
+     return;
     }
- let result = compute(previousValue, currentOperator, currentValue);
- if(currentOperator == ""){
+    if(currentOperator === "x"){
+        calcOperator = "multiply";
+    }
+    else if(currentOperator === "÷"){
+        calcOperator = "divide";
+    }
+    else if(minusCount % 2 === 0){
+        calcOperator = "add";
+    }
+    else{
+        calcOperator = "subtract";
+    }
+    
+    let result = compute(previousValue, calcOperator, currentValue);
+    if(currentOperator == ""){
     return
- }
- else if(currentValue== ""){
+   }
+    else if(currentValue== ""){
     currentValue = previousValue;
+   }
+    if(isNaN(result)){
+      currentOperator = "";
+      previousValue = "";
+      currentValue = "";
+      updateDisplay("Syntax Error");
+   }
+   else{
+      currentOperator = "";
+      previousValue = String(result);
+      currentValue = "";
+      updateDisplay(result);
+   }
  }
- if(isNaN(result)){
-    currentOperator = "";
-    previousValue = "";
-    currentValue = "";
-    updateDisplay("Syntax Error");
- }
- else{
- currentOperator = "";
- previousValue = String(result);
- currentValue = "";
- updateDisplay(result);
-}
-}
-let = clearAll = () => {
+   let = clearAll = () => {
     currentValue = '';
     previousValue = '';
     currentOperator = '';
     updateDisplay(0);
-}
+ }
 
-let deleteDigit = () => {
+   let deleteDigit = () => {
     if(currentValue == ""){
         currentValue ="";
         currentOperator = "";
@@ -103,16 +128,19 @@ let deleteDigit = () => {
         previousValue ="";
         updateDisplay();
     }
+    if(currentOperator == [...currentOperator]){
+        currentOperator = currentOperator.slice(0, -1);
+    }
     else{currentValue = currentValue.slice(0, -1);
     updateDisplay(currentValue);
-}
-}
-let handleDecimal = () => {
+  }
+ }
+   let handleDecimal = () => {
     if(!currentValue.includes(".")){
         currentValue += ".";
         updateDisplay();
     }
-}
+ }
  percent.addEventListener('click', () => {
    if(currentValue ==""){
     currentValue = previousValue;
@@ -123,7 +151,30 @@ let handleDecimal = () => {
    updateDisplay();
    
  })
-
+ sd.addEventListener('click', () => {
+     if(currentValue ==""){
+    currentValue = previousValue;
+    previousValue = "";
+   }
+   let digitsCount,  dotIndex;
+   if(currentValue.includes(".")){
+     dotIndex = currentValue.indexOf(".");
+     digitsCount = currentValue.length - dotIndex -1;
+     let denominator = 10 ** digitsCount;
+     currentValue = currentValue*(10**digitsCount);
+     let gcd = findGCD(currentValue, denominator);
+     currentValue = currentValue/gcd;
+     denominator = denominator/gcd;
+    currentValue = currentValue + "/" + denominator;
+    updateDisplay();
+   }
+   else if(currentValue.includes("/")){
+    let numerator, parts;
+    parts = currentValue.split("/");
+    currentValue = String(parts[0]/parts[1]);
+    updateDisplay();
+   }
+ })
 btns.forEach((button) => {
     button.addEventListener('click', () =>{
         if([...num].includes(button)){
