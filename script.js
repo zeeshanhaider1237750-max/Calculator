c2fb921743a51340563d413ecc7b417532e66c54
Zeeshan Haider <ZeeshanHaider.1237750@gmail.com>
2026-06-19 18:17:18 +0500

    keyboard support and long decimals

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ let currentValue = "";
 let previousValue = "";
 let currentOperator = "";
 let displaySymbol = "";
+let shouldDisplayResult = false;
 
 let findGCD = (a, b) => {
     while (b !== 0) {
@@ -37,8 +38,14 @@ let compute = (num1, operator, num2) => {
         return  num1 * num2;
     }
     else if(operator == "÷"){
+        if(num2 === 0 && operator == "÷"){
+            return "Snarky Error";
+        }
+       else{ 
         return  num1 / num2;
+       }
     }
+    updateDisplay();
 }
 
 let updateDisplay = (text) => {
@@ -50,6 +57,12 @@ let updateDisplay = (text) => {
    }
 
 let handleDigit = (digit) => {
+    if(shouldDisplayResult == true){
+    currentOperator = "";
+    currentValue = "";
+    previousValue = "";
+    shouldDisplayResult = false;
+    }
     currentValue += digit;
     updateDisplay();
 }
@@ -59,6 +72,16 @@ let handleOperator =(nextOperator, nextOperatorSymbol) => {
        
     if(currentValue !=="" && currentOperator !=="" && previousValue !== ""){
         let result = compute(previousValue, currentOperator, currentValue);
+        if(result == "Snarky Error"){
+            currentValue = "";
+            currentOperator = "";
+            previousValue = "";
+            updateDisplay("Snarky Error");
+            return
+        }
+         if(result % 1 !== 0){
+    result = Math.round(result*10000)/10000;
+   }
         previousValue = String(result);
         currentValue = "";
         currentOperator = nextOperatorSymbol;
@@ -71,9 +94,15 @@ let handleOperator =(nextOperator, nextOperatorSymbol) => {
     else{
        currentOperator = nextOperatorSymbol
        }
-       updateDisplay(); 
+       updateDisplay();
 } 
 let handleEquals = () => {
+     if(currentValue.includes('.') && currentOperator == "" && previousValue == ""){
+    currentValue = String(Math.round(parseFloat(currentValue) *10000)/10000);
+    updateDisplay(currentValue);
+    shouldDisplayResult = true;
+    return
+   }
     let addCount = [...currentOperator].filter(char => char === "+").length;
     let minusCount = [...currentOperator].filter(char => char === "-").length;
     let errorCount = [...currentOperator].filter(char =>char === "x" ||  char === "÷").length;
@@ -105,6 +134,16 @@ let handleEquals = () => {
     else if(currentValue== ""){
     currentValue = previousValue;
    }
+   if(result == "Snarky Error"){
+    currentValue = "";
+    currentOperator = "";
+    previousValue = "";
+    updateDisplay("Snarky Error");
+    return
+   }
+    if(result % 1 !== 0){
+    result = Math.round(result*10000)/10000;
+   }
     if(isNaN(result)){
       currentOperator = "";
       previousValue = "";
@@ -116,6 +155,7 @@ let handleEquals = () => {
       previousValue = String(result);
       currentValue = "";
       updateDisplay(result);
+      shouldDisplayResult = true;
    }
  }
    let = clearAll = () => {
@@ -123,39 +163,37 @@ let handleEquals = () => {
     previousValue = '';
     currentOperator = '';
     updateDisplay(0);
+    shouldDisplayResult = true;
  }
 
    let deleteDigit = () => {
-    if(currentValue == ""){
-        currentValue ="";
-        currentOperator = "";
-        currentValue = previousValue.slice(0,-1);
-        previousValue ="";
-        updateDisplay();
-    }
-    if(currentOperator == [...currentOperator]){
-        currentOperator = currentOperator.slice(0, -1);
-    }
-    else{currentValue = currentValue.slice(0, -1);
-    updateDisplay(currentValue);
+  if(currentValue.length > 0){
+    currentValue = currentValue.slice(0, -1);
   }
+  else if(currentOperator.length > 0){
+    currentOperator = currentOperator.slice(0, -1);
+  }
+  else if(previousValue.length > 0){
+    previousValue = previousValue.slice(0, -1);
+  }
+  updateDisplay();
  }
+
    let handleDecimal = () => {
     if(!currentValue.includes(".")){
         currentValue += ".";
         updateDisplay();
     }
  }
- percent.addEventListener('click', () => {
-   if(currentValue ==""){
+ let percentageCalc = () => {
+    if(currentValue ==""){
     currentValue = previousValue;
     previousValue = "";
    }
    currentValue = currentValue/100;
    currentValue = String(currentValue);
    updateDisplay();
-   
- })
+ }
 
 btns.forEach((button) => {
     button.addEventListener('click', () =>{
@@ -177,5 +215,63 @@ else if(button===del){
 else if(button===decimal){
      handleDecimal();
 }
+else if(button===percent){
+    percentageCalc();
+}
     });
 });
+window.addEventListener('keydown', (e) => {
+   if(e.key === "0"){
+    handleDigit("0");
+}
+else if(e.key === "1"){
+    handleDigit("1");
+}
+else if(e.key === "2"){
+    handleDigit("2");
+}
+else if(e.key === "3"){
+    handleDigit("3");
+}
+else if(e.key === "4"){
+    handleDigit("4");
+}
+else if(e.key === "5"){
+    handleDigit("5");
+}
+else if(e.key === "6"){
+    handleDigit("6");
+}
+else if(e.key === "7"){
+    handleDigit("7");
+}
+else if(e.key === "8"){
+    handleDigit("8");
+}
+else if(e.key === "9"){
+    handleDigit("9");
+}
+ else if(e.key === "+"){
+  handleOperator("add", "+");
+}
+else if(e.key === "-"){
+    handleOperator("subtract", "-");
+}
+else if(e.key === "*"){
+    handleOperator("multiply", "x");
+}
+else if(e.key === "/"){
+    handleOperator("divide", "÷");
+}
+else if(e.key === "Enter"){
+   handleEquals();
+}
+else if(e.key === "Backspace"){
+    deleteDigit();
+ }
+ else if(e.key === "Delete"){
+    clearAll();
+ }
+ else if
+ })
+
